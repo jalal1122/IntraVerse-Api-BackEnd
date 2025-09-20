0dec54f30ff44a8b54eeb73b73e450eacb740ded
muhammad uzair <abuzada735@gmail.com>
2025-09-20 15:30:23 +0500

    fix: Update CORS configuration to include additional origin

--- src/app.js
+++ src/app.js
@@ -16,7 +16,11 @@ const app = express();
 // Configure CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://intraaverse.netlify.app"], // Vite dev server ports
+    origin: [
+      "http://localhost:5173",
+      "https://intraaverse.netlify.app",
+      "https://intraverse.me",
+    ], // Vite dev server ports
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
